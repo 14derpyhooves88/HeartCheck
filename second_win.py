f1b7578007df3107e45f3cca2893382f0a077cc1
14derpyhooves88 <artem2004620@gmail.com>
2022-03-27 14:43:50 +0300

    mod

--- second_win.py
+++ second_win.py
@@ -1,1 +1,29 @@
-# напиши здесь код для второго экрана приложения
+
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont
+from PyQt5.QtWidgets import (
+    QApplication, QWidget,
+    QHBoxLayout, QVBoxLayout, QGridLayout,
+    QGroupBox, QRadioButton,
+    QPushButton, QLabel, QListWidget, QLineEdit)
+class TestWin(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    
+    def set_appear(self):
+        pass
+
+    def initUI(self):
+        self.h_line = QHBoxLayout()
+        self.r_line = QVBoxLayout()
+        self.l_line = QVBoxLayout()
+        self.l_line.addWidget(self.btn1)
+        self.r_line.addWidget(self.timer)
+        self.h_line.addLayout(self.l_line)
+        self.h_line.addLayout(self.r_line)
+        self.setLayout(self.h_line)
